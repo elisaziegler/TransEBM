2d6cf260ddd99d23cba525e66ac050bce78da63a
eziegler <eziegler@iup.uni-heidelberg.de>
2020-06-08 18:43:59 +0200

    added comments for changes that might be necessary to run the model

--- config/config_module.sh
+++ config/config_module.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# changes to run the model:
+# 1) change compiler gfortran-7 to compiler of choice
 gfortran-7 -c parse_config.f90 -o ../config/parse_config.o
 
 cp parse_config.o ../preprocess/
--- preprocess/preprocess.sh
+++ preprocess/preprocess.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# change to make to run model: 
+# 1) change -I\usr\include to refer to the include folder for netcdf for Fortran on your system
+# 2) if lnetcdff doesn't exist as a flag for the netcdf for Fortran library in your system, remove it and use: -L/home/username/netcdf/lib and the commented line instead to provide the path to the netcdf library
+# 3) change gfortran-7 to Fortran compiler of choice
+
 gfortran-7 -c ../config/parse_config.f90 -o ../config/parse_config.o
 cp ../config/parse_config.o ../src/
 cp ../config/parse_config.o ../preprocess/
@@ -7,6 +12,7 @@ cp ../config/configuration_parser.mod ../src/
 cp ../config/configuration_parser.mod ../preprocess/
 
 gfortran-7 ../config/parse_config.f90 extract.f90 prepare_albedo.f90 prepare_geography.f90 preprocessing.f90 -o preprocessing -I/usr/include -lnetcdff
+#gfortran-7 ../config/parse_config.f90 extract.f90 prepare_albedo.f90 prepare_geography.f90 preprocessing.f90 -o preprocessing -I/usr/include -L/home/username/netcdf/lib
 
 ./preprocessing
 mv -f *.nc ../input
